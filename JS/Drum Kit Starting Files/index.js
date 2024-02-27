40ceb722159a5bf60bf970b31570668328fe7785
patilnikhil805 <pnikhil.805@gmail.com>
2024-02-26 21:58:38 -0500

    Chapter 145

--- JS/Drum Kit Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
-
-    document.querySelector(".drum")[i].addEventListener("click",function () {
-        alert("clicked");
-    });
-
-};
